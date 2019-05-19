3bdf8b37d91b14c8c4bdc0ef7e0f8c00e23514d9
DanielBergshoeff <dabergshoeff@gmail.com>
2019-05-19 21:24:43 +0200

    Added different stages for enemy

---
+++ Assets/scripts/BehaviourTree/EnemyManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager Instance { get; private set; }
+    public static SoundUnityEvent soundUnityEvent;
+
+    private List<BehaviourTree> enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null) {
+            Instance = this;
+            if (soundUnityEvent == null)
+                soundUnityEvent = new SoundUnityEvent();
+        }
+
+        soundUnityEvent.AddListener(OnSound);
+        enemies = new List<BehaviourTree>();
+    } 
+
+    public static void AddEnemy(BehaviourTree enemy) {
+        Instance.enemies.Add(enemy);
+    }
+
+    private void OnSound(float range, float loudness, Vector3 pos) {
+        foreach (BehaviourTree enemy in enemies) {
+            if ((pos - enemy.transform.position).sqrMagnitude <= range * range) {
+                enemy.IncreaseAnger(loudness);
+            }
+        }
+    }
+}
---
+++ Assets/scripts/BehaviourTree/SoundObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundObject : MonoBehaviour
+{
+    public float Range = 10.0f;
+    public float Loudness = 1.0f;
+    public AudioClip Sound;
+
+    private float waitForPlay = 0.0f;
+    private AudioSource myAudioSource;
+
+    private void Start() {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+        if (waitForPlay > 0f)
+            waitForPlay -= Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.CompareTag("Player")) {
+            if (waitForPlay <= 0f) {
+                EnemyManager.soundUnityEvent.Invoke(Range, Loudness, transform.position);
+                myAudioSource.PlayOneShot(Sound);
+                waitForPlay = Sound.length;
+            }
+        }
+    }
+}
+
+public class SoundUnityEvent : UnityEvent<float, float, Vector3> { }
