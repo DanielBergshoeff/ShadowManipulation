1aef3a3407abe2a8cf7be23ce6c75b71fac6d948
DanielBergshoeff <dabergshoeff@gmail.com>
2019-06-09 20:36:10 +0200

    Monster2 current animations

--- Assets/scripts/BehaviourTree/BehaviourTreeMonsterTwo.cs
+++ Assets/scripts/BehaviourTree/BehaviourTreeMonsterTwo.cs
@@ -18,6 +18,7 @@ public class BehaviourTreeMonsterTwo : MonoBehaviour
     private Vector3 heightRemoved;
     private NavMeshAgent myNavMeshAgent;
     private bool attacking = false;
+    private bool aboveGround = false;
 
     private Attackable Target;
     private Animator myAnimator;
@@ -32,22 +33,29 @@ public class BehaviourTreeMonsterTwo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.Instance.LightMovementScript.Height <= MaxLightHeight) {
+        if(GameManager.Instance.LightMovementScript.Height <= MaxLightHeight && Vector3.Distance(new Vector3(GameManager.Instance.LightObject.transform.position.x, transform.position.y, GameManager.Instance.LightObject.transform.position.z), transform.position) <= ViewRange && !attacking) {
             lightDownTimer += Time.deltaTime;
             if (lightDownTimer >= TimeLightDownToThrow) {
                 Target = GameManager.Instance.LightObject.GetComponent<Attackable>();
             }
         }
 
-        if (Target == null)
-            return;
+        if (Target == null) {
+            if(Vector3.Distance(transform.position, new Vector3(GameManager.Instance.Player.transform.position.x, transform.position.y, GameManager.Instance.Player.transform.position.z)) <= ViewRange) {
+                Target = GameManager.Instance.LightObject.GetComponent<Attackable>();
+            }
+            else
+                return;
+        }
 
         heightRemoved = new Vector3(Target.transform.position.x, transform.position.y, Target.transform.position.z);
 
         if (attacking) {
+            transform.position = heightRemoved;
             transform.LookAt(heightRemoved);
             return;
         }
+
 
         if (Target.gameObject == GameManager.Instance.LightObject) { //If the Light is the target
             if(Vector3.Distance(transform.position, heightRemoved) > LightAttackDistance + AttackDistanceMargin || Vector3.Distance(transform.position, heightRemoved) < LightAttackDistance - AttackDistanceMargin) {
@@ -57,9 +65,9 @@ public class BehaviourTreeMonsterTwo : MonoBehaviour
                 myNavMeshAgent.SetDestination(targetPosition);
                 myAnimator.SetFloat("Speed", myNavMeshAgent.speed);
             }
-            else {
+            else if(!attacking){
                 myNavMeshAgent.speed = 0f;
-                myNavMeshAgent.enabled = false;
+                //myNavMeshAgent.enabled = false;
                 myAnimator.SetFloat("Speed", myNavMeshAgent.speed);
                 myAnimator.SetTrigger("HitUp");
                 attacking = true;
@@ -73,9 +81,9 @@ public class BehaviourTreeMonsterTwo : MonoBehaviour
                 myNavMeshAgent.SetDestination(targetPosition);
                 myAnimator.SetFloat("Speed", myNavMeshAgent.speed);
             }
-            else {
+            else if(!attacking){
                 myNavMeshAgent.speed = 0f;
-                myNavMeshAgent.enabled = false;
+                //myNavMeshAgent.enabled = false;
                 myAnimator.SetFloat("Speed", myNavMeshAgent.speed);
                 myAnimator.SetTrigger("GrabPlayer");
                 attacking = true;
@@ -83,9 +91,15 @@ public class BehaviourTreeMonsterTwo : MonoBehaviour
         }
         
     }
-    
+
+    public void StopAttack() {
+        attacking = false;
+        lightDownTimer = 0f;
+        Target = GameManager.Instance.Player.GetComponent<Attackable>();
+    }
+
 
     public void ThrowLightUp() {
-        Target = GameManager.Instance.Player.GetComponent<Attackable>();
+        GameManager.Instance.LightMovementScript.ThrowUp();
     }
 }
